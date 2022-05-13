d07a387e28d7440bc2e3e8c63859cc6bdb366e19
Lam Thai Thanh Long <lamthaithanhlong@gmail.com>
2022-05-13 02:11:00 -0500

    update message and socket server service

--- server.go
+++ server.go
@@ -12,3 +12,41 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+func (s *WsServer) Start(addr string) error {
+	router := gin.Default()
+	router.GET("/health", func(context *gin.Context) {
+		context.JSON(200, nil)
+	})
+	var handlers []gin.HandlerFunc
+	handlers = append(handlers, s.Middlewares...)
+	handlers = append(handlers, func(context *gin.Context) {
+		upgrader.CheckOrigin = func(r *http.Request) bool {
+			return true
+		}
+		conn, err := upgrader.Upgrade(context.Writer, context.Request, nil)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		client := NewClientConnection(context, conn)
+
+		client.onClose = func() {
+			if s.onCloseConnection != nil {
+				s.onCloseConnection(client)
+			}
+		}
+		client.onMessage = func(message Message) {
+			if s.onMessage != nil {
+				s.onMessage(message, client)
+			}
+		}
+
+		go client.read()
+
+		s.onConnection(client)
+	})
+	router.Any("/ws/socket", handlers...)
+
+	return router.Run(addr)
+}
+
